f20d0a49e28bb294b7a2236e051390eba2a9787d
Reacoder <sampythoner@gmail.com>
2018-11-29 22:30:00 +0800

    finish ths data fetch.

--- engine.py
+++ engine.py
@@ -14,7 +14,7 @@ from private.account import LXR
 
 driver = webdriver.Chrome()
 lxr = LXR()
-FACTOR = 2  # 分辨率高的为2，低的为1
+FACTOR = 1  # 分辨率高的为2，低的为1
 
 
 def save_image(canvas, code, filename):
@@ -187,14 +187,24 @@ def grab_ths_operate(code):
     driver.implicitly_wait(30)
     element_list = driver.find_elements_by_css_selector(".bd.pt5")
     # 公司产品
-    element = element_list[2]
+    # element = element_list[2]
+    # scroll_to_element(element)
+    # hide_float_search()
+    # save_element(element, code, 'img_operate_product')
+
+    elements = driver.find_elements_by_css_selector(".m_table.m_hl")
+    element = elements[2]
     scroll_to_element(element)
     hide_float_search()
-    save_element(element, code, 'img_product_category')
+    save_element(element, code, 'img_operate_product')
+    element = elements[3]
+    scroll_to_element(element)
+    save_element(element, code, 'img_operate_product2')
+
     # 上下游
     element = element_list[3]
     scroll_to_element(element)
-    save_element(element, code, 'img_partner')
+    save_element(element, code, 'img_operate_partner')
     # 董事会经营评述
     driver.find_elements_by_css_selector(".more.fr")[0].click()
     driver.implicitly_wait(10)
@@ -225,7 +235,10 @@ def grab_ths_holder(code):
     element = element_list[3]
     scroll_to_element(element)
     log(element.location['y'])
-    save_element(element, code, 'img_controller', y=element.size['width'] * 0.35)
+    # 公司
+    # save_element(element, code, 'img_controller', y=element.size['width'] * 0.35)
+    # 家里
+    save_element(element, code, 'img_controller', y=element.size['width'] * 0.25)
 
 
 def grab_ths_worth(code):
@@ -239,10 +252,17 @@ def grab_ths_worth(code):
     scroll_to_element(element)
     hide_float_search()
     save_element(element, code, 'img_worth_forecast')
+    # driver.execute_script("document.body.style.zoom='80%'")
     # 业绩预测详表
-    element = element_list[2]
+    # element = element_list[2]
+    # scroll_to_element(element)
+    # save_element(element, code, 'img_worth_forecast_table')
+    element = driver.find_elements_by_css_selector(".m_table.m_hl.posi_table")[0]
     scroll_to_element(element)
     save_element(element, code, 'img_worth_forecast_table')
+    element = driver.find_elements_by_css_selector(".m_table.m_hl.ggintro.ggintro_1.organData")[0]
+    scroll_to_element(element)
+    save_element(element, code, 'img_worth_forecast_table2')
 
 
 def grab_ths_news(code):
@@ -255,12 +275,11 @@ def grab_ths_news(code):
     # 研报评级
     tr_list = driver.find_elements_by_css_selector(".organ_item")
 
-    # link_list = driver.find_elements_by_css_selector(".client.pagescroll")
     for tr in tr_list:
         link = tr.find_element_by_css_selector(".client.pagescroll")
         td_list = tr.find_elements_by_tag_name('td')
-        title = td_list[1].text + " " * 6 + td_list[2].text + " " * 6 + td_list[3].text + "\n"
-        title = title + "=" * 40 + "\n"
+        title = td_list[1].text + "\n" + td_list[2].text + "\n" + td_list[3].text + "\n"
+        title = title + "-" * 50 + "\n"
         save_file(code, 'file_worth', title)
 
         window_before = driver.window_handles[0]
@@ -269,9 +288,60 @@ def grab_ths_news(code):
         window_after = driver.window_handles[1]
         driver.switch_to.window(window_after)
         element = driver.find_element_by_css_selector(".YBText")
-        save_file(code, 'file_worth', element.text)
+        save_file(code, 'file_worth', element.text + "\n\n" + "=" * 50 + "\n\n")
         driver.switch_to.window(window_before)
         driver.implicitly_wait(10)
+
+
+def grab_ths_concept(code):
+    log('概念题材')
+    url = 'http://basic.10jqka.com.cn/%s/concept.html' % code
+    driver.get(url)
+    driver.implicitly_wait(30)
+    driver.find_elements_by_css_selector(".conAllBtn")[2].click()
+    driver.implicitly_wait(3)
+    element = driver.find_element_by_id("material")
+    save_file(code, 'file_concept', element.text)
+
+
+def grab_ths_position(code):
+    log('主力持仓')
+    url = 'http://basic.10jqka.com.cn/%s/position.html' % code
+    driver.get(url)
+    driver.implicitly_wait(30)
+    element_list = driver.find_elements_by_css_selector(".bd.pt5.pr")
+    element = element_list[0]
+    scroll_to_element(element)
+    hide_float_search()
+    save_element(element, code, 'img_position')
+
+
+def grab_ths_bonus(code):
+    log('分红融资')
+    url = 'http://basic.10jqka.com.cn/%s/bonus.html' % code
+    driver.get(url)
+    driver.implicitly_wait(30)
+    element = driver.find_element_by_id("bonus_table")
+    scroll_to_element(element)
+    hide_float_search()
+    save_element(element, code, 'img_bonus')
+    element = driver.find_element_by_id("additionprofile_bd")
+    scroll_to_element(element)
+    save_element(element, code, 'img_addition_profile')
+
+
+def grab_ths_event(code):
+    log('公司大事')
+    url = 'http://basic.10jqka.com.cn/%s/event.html' % code
+    driver.get(url)
+    driver.implicitly_wait(30)
+    element = driver.find_element_by_id("manager")
+    scroll_to_element(element)
+    hide_float_search()
+    save_element(element, code, 'img_event_manager')
+    element = driver.find_element_by_id("holder")
+    scroll_to_element(element)
+    save_element(element, code, 'img_event_holder')
 
 
 def save_element(element, code, filename, y=0):
@@ -292,19 +362,24 @@ def scroll_to_element(element):
 def hide_float_search():
     head = driver.find_element_by_class_name('header')
     driver.execute_script('$(arguments[0]).fadeOut()', head)
-    search = driver.find_element_by_css_selector('.searchfx')
+    search = driver.find_element_by_css_selector('.iwc_searchbar.clearfix.float_box')
     driver.execute_script('$(arguments[0]).fadeOut()', search)
     time.sleep(1)
 
 
 if __name__ == '__main__':
     code = '600373'
+    # driver.execute_script("document.body.style.zoom='80%'")
     # driver.maximize_window()
     # grab_lxr_data(code)
     # grab_ths_trend(code)
-    # grab_ths_brief(code)
-    # grab_ths_operate(code)
-    # grab_ths_holder(code)
-    # grab_ths_worth(code)
+    grab_ths_brief(code)
+    grab_ths_operate(code)
+    grab_ths_holder(code)
+    grab_ths_worth(code)
     grab_ths_news(code)
+    grab_ths_concept(code)
+    grab_ths_position(code)
+    grab_ths_bonus(code)
+    grab_ths_event(code)
     log('大功告成')
